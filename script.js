1f9bca3353118828bb6a6d89995745be1e9e6693
Priyam Jaiswal <priyamj374@gmail.com>
2024-12-11 21:02:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const Loader=document.querySelector("#Game-Container");
+let keys=document.querySelectorAll(".Game-btns");
+let restarter=document.querySelector("#Restart-btn");
+let loosePopUp=document.querySelector("#Winning-Pop-Up");
+let fullPage=document.querySelector("#body");
+let gameOverStatement=document.querySelector("#Loosing-Text");
+let gameOverKey;
+let score=0;
+let scoreReader=document.querySelector("#score");
+restarter.addEventListener("click",()=>
+{
+document.location.href="index.html";
+})
+document.addEventListener("DOMContentLoaded",()=>
+{
+    gameOverKey=Math.floor(Math.random()*20)+1;
+    console.log(gameOverKey);
+})
+keys.forEach(key => {
+key.addEventListener("click",()=>{
+if(key.id==gameOverKey)
+{
+loosePopUp.style.display="block";
+key.style.backgroundColor="red";
+fullPage.style.pointerEvents="none";
+loosePopUp.style.pointerEvents="auto";
+gameOverStatement.innerText+=score;
+}
+else
+{
+key.style.backgroundColor="green";
+score++;
+scoreReader.innerHTML=score;
+key.disabled=true;
+}
+})
+});
